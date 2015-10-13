0ec0add71b10a8f2e0116dea63f051ae161d1859
sdiemert <simon.diemert@gmail.com>
2015-10-13 23:33:02 +0000

    Updated install script

--- util/nrpe/deploy_monitoring_scripts.sh
+++ util/nrpe/deploy_monitoring_scripts.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # This script deploys the scripts required for NRPE to monitor endpoints.
+#
+# !!!!!!!!!!!!!!!!! THIS SCRIPT WILL REMOVE ANY EXISTING NRPE CONFIGURATIONS !!!!!!!!!!!!!!!!!!!!!!!
 # 
 # Assumptions: 
 # 	
@@ -18,6 +20,22 @@
 #	$ deploy_monitoring_scripts.sh 1 2 3 4 5
 #
 # Will set up the scripts for endpoints 1 through 5
+
+echo ""
+echo ""
+echo "---------------------------------------------------"
+echo "THIS SCRIPT WILL REMOVE ANY EXISTING NRPE CONFIGURATIONS"
+read -p "Press ENTER to continue, CTRL-C to halt."
+echo "---------------------------------------------------"
+echo ""
+echo ""
+
+if [ $# == 0 ]; then
+	echo "ERROR: This script takes endpoint number arguments, $# were provided, exiting..."
+	echo ""
+	exit 1
+	
+fi
 
 # Set up some variables
 
@@ -37,16 +55,36 @@ cp -r $BASE_DIR""monitoring_scripts/* $SCRIPT_DEPLOY_DIR
 
 # 3) Set up the commands in /etc/nagios/nrpe_local.cfg, we use another script to support this.
 
+# 3.1) Remove existing config file
+
+rm -rf $NRPE_CFG_FILE
+touch $NRPE_CFG_FILE
+
 for ep in $@ #gets all arguments to the script.
 do
+	echo ""
 	echo "Configuring scripts for endpoint: "$ep
+	echo "---------------------------------"
 
 	for f in $SCRIPT_DEPLOY_DIR/* #get all executable filese
 	do
 		if [[ -x $f ]]; then
-			./add_nrpe_command.sh $NRPE_CFG_FILE $f $ep
+			./add_nrpe_command.sh $f $ep $NRPE_CFG_FILE
 		fi
 	done
+	echo "================================"
 done
+
+# 4) Restart nagios-nrpe server
+
+echo ""
+echo "Restarting NRPE Server: "
+echo "------------------------"
+
+sudo service nagios-nrpe-server restart
+
+echo ""
+echo "-----------------------"
+echo "All Done, Exiting..."
 
 exit 0
--- util/nrpe/install-nrpe.sh
+++ util/nrpe/install-nrpe.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
+NRPE_PORT=3010 
+NRPE_CONFIG=/etc/nagios/nrpe.cfg
+
+# install the NRPE service
+
 sudo apt-get update
 sudo apt-get install nagios-nrpe-plugin -y 
+
+# Set the port for NRPE to listen on: 
+
+sed -i 's/server_port=[0-9]\+/server_port='$NRPE_PORT'/g' $NRPE_CONFIG
+
+# Make the /usr/local/lib/nagios directory for storing plugin scripts
+
+mkdir -p /usr/local/lib/nagios
+
+# Make a nrpe_local.cfg file if it does not already exist
+
+touch /etc/nagios/nrpe_local.cfg 

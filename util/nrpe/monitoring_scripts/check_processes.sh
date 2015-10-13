b1bef5eacf76dea9b4e84b25b0c887f819a24e7f
sdiemert <simon.diemert@gmail.com>
2015-10-13 21:26:04 +0000

    Added scripts executing nagios-nrpe checks.

---
+++ util/nrpe/monitoring_scripts/check_processes.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# This script takes 1 argument, the endpoint number to check. 
+
+# Check the number of arguments and make sure it is as expected. 
+if [ $# -ne 1 ]; then 
+	echo ""
+	echo "ERROR: Script takes exactly 1 argument, the endpoint to connect to."
+	echo "Sample usage: $ ./check_processes.sh 5"
+	echo ""
+	exit 1; 
+fi
+
+
+# Get the port by adding the endpoint number base port.
+PORT=$((40000+$1)) 
+
+URL="http://localhost:"$PORT"/sysinfo/processes"
+TMPFILE=`/bin/tempfile`
+
+# Expect two lines of output from call to web server
+CMD=`/usr/bin/curl -sSf $URL > $TMPFILE 2>&1`
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+  /bin/echo "UNKNOWN - For $URL got $(cat $TMPFILE)"
+  /bin/rm $TMPFILE
+  exit 3
+fi
+# first line should be Nagios message
+NAGIOSMSG=`/usr/bin/head -1 $TMPFILE`
+# second line should be Nagios status code
+STATUSSTR=`/bin/cat $TMPFILE | /usr/bin/head -2 | /usr/bin/tail -1`
+/bin/rm $TMPFILE
+NAGIOSSTATUS=-1
+if [ "$STATUSSTR" == "Status Code: 0" ]; then
+  NAGIOSSTATUS=0
+fi
+if [ "$STATUSSTR" == "Status Code: 1" ]; then
+  NAGIOSSTATUS=1
+fi
+if [ "$STATUSSTR" == "Status Code: 2" ]; then
+  NAGIOSSTATUS=2
+fi
+if [ "$STATUSSTR" == "Status Code: 3" ]; then
+  NAGIOSSTATUS=3
+fi
+if [ "$NAGIOSSTATUS" -ge 0 -a "$NAGIOSSTATUS" -le 3 ]; then
+  echo $NAGIOSMSG
+  exit $NAGIOSSTATUS
+else
+  # shouldn't get here unless there was a completely unexpected response
+  /bin/echo "UNKNOWN - Unexpected response from \"$URL\". Return status $STATUS"
+  exit 3
+fi

63e3ab452250b4dd3639a237583b069ec805a89c
Derek Roberts <derek.roberts@gmail.com>
2016-01-18 15:27:58 -0800

    run_batch_queries.sh run w/o being in the same dir

--- util/run_batch_queries.sh
+++ util/run_batch_queries.sh
@@ -3,6 +3,12 @@
 set -e -o nounset
 
 
+# Find and chanbge to script directory
+#
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${DIR}
+
+
 # Prep job_params, if necessary
 #
 [ -s ./job_params/job_params.json ]|| \
